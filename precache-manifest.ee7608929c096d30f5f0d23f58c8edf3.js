b1c91d9c9dc5652faf381002eb70b344db30b7c9
mithun <49356216+MithunMi97@users.noreply.github.com>
2020-08-12 14:15:01 +0530

    Updates

---
+++ precache-manifest.ee7608929c096d30f5f0d23f58c8edf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605ed993237af3e4779c0a6106a73b9e",
+    "url": "/Reactjs-Todo-List/index.html"
+  },
+  {
+    "revision": "210c89093fdfe890b607",
+    "url": "/Reactjs-Todo-List/static/css/main.0c17913a.chunk.css"
+  },
+  {
+    "revision": "3a83a75e35143e1a572d",
+    "url": "/Reactjs-Todo-List/static/js/2.8bc66dc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactjs-Todo-List/static/js/2.8bc66dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210c89093fdfe890b607",
+    "url": "/Reactjs-Todo-List/static/js/main.9f2d50f0.chunk.js"
+  },
+  {
+    "revision": "8393cc778fda55faccc8",
+    "url": "/Reactjs-Todo-List/static/js/runtime-main.6a2f4e09.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Reactjs-Todo-List/static/media/logo.ee7cd8ed.svg"
+  }
+]);
